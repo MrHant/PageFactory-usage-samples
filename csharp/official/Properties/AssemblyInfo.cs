5f29a52b2d2fae9d0d254b0824047e191b22c502
Artyom Silivonchik <mr.hant@gmail.com>
2018-10-09 00:29:01 +0300

    Add official sample in csharp

---
+++ csharp/official/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("31BDF36B-00DF-4220-A30E-90B8FF6E1161")]
+
+[assembly: AssemblyVersion("1.0.*")]
