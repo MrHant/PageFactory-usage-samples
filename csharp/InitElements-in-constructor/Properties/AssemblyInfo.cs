43b3da69384e705a3de932a63b3c86708b7bbdff
Artyom Silivonchik <mr.hant@gmail.com>
2018-10-11 02:37:48 +0300

    Add InitElements-in-constructor implementation for csharp

---
+++ csharp/InitElements-in-constructor/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("09005757-9F44-491F-99A7-AAF38659E6D1")]
+
+[assembly: AssemblyVersion("1.0.*")]
---
+++ csharp/InitElements-in-constructor/Tests.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.PageObjects;
+using Tiver.Fowl.Drivers.Downloaders;
+
+namespace InitElements_in_constructor
+{
+    public class GoogleSearchPage {
+        [FindsBy(How = How.Name, Using = "q")]
+        private IWebElement _queryField;
+
+        public GoogleSearchPage(IWebDriver driver)
+        {
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void SearchFor(string text) {
+            _queryField.SendKeys(text);
+            _queryField.Submit();
+        }
+    }
+
+    public class GoogleResultsPage {
+        [FindsBy(How = How.TagName, Using = "h3")]
+        private IList<IWebElement> _searchResults;
+
+        public GoogleResultsPage(IWebDriver driver)
+        {
+            PageFactory.InitElements(driver, this);
+        }
+
+        public IEnumerable<string> SearchResultsTitles()
+        {
+            return _searchResults.Select(r => r.Text);
+        }
+    }
+    
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void EnterTextInGoogleSearchInput()
+        {
+            // download driver binary
+            new ChromeDriverDownloader().DownloadBinary("LATEST_RELEASE");
+            
+            // launch driver
+            var driver = new ChromeDriver();
+              
+            
+            // open Google start page page
+            driver.Navigate().GoToUrl("https://google.com");
+            
+            // init elements
+            var page = new GoogleSearchPage(driver);
+
+            // do the search.
+            page.SearchFor("Cheese");
+            
+            // list found results
+            var resultsPage = new GoogleResultsPage(driver);
+            var results = resultsPage.SearchResultsTitles();
+            
+            // assert that there are 10 results
+            Assert.IsTrue(results.Count().Equals(10));
+            
+            
+            // close driver
+            driver.Quit();
+        }
+    }
+}
