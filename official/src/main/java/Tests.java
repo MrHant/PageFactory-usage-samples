c451ea2bba79f36a24a98a7c0dff0450eabd6661
Artyom Silivonchik <mr.hant@gmail.com>
2018-10-15 07:59:20 +0300

    Fix dependencies, add comments, add WebDriverManager

--- official/src/main/java/Tests.java
+++ official/src/main/java/Tests.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
@@ -11,19 +12,31 @@ public class Tests {
 
     @Test
     public void enterTextInGoogleSearchInput(){
+        // download driver binary
+        WebDriverManager.chromedriver().setup();
+
+        // launch driver
         WebDriver driver = new ChromeDriver();
 
+
+        // open Google start page page
         driver.navigate().to("https://google.com");
 
+        // init elements
         GoogleSearchPage googleSearchPage = PageFactory.initElements(driver, GoogleSearchPage.class);
 
+        // do the search.
         googleSearchPage.searchFor("Cheese");
 
+        // list found results
         GoogleResultsPage googleResultsPage = PageFactory.initElements(driver, GoogleResultsPage.class);
         List<String> results = googleResultsPage.searchResultsTitles();
 
+        // assert that there are 9 results
         Assertions.assertEquals(9, results.size());
 
+
+        // close driver
         driver.quit();
     }
 }
